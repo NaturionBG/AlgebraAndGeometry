df01f8284c6a9519241ce9de6b2f15966b24155d
Naturion <naturionprodudu@mail.ru>
2024-12-30 15:18:20 +0300

    Added a function for Matrix Operations question.

--- MatCalc.py
+++ MatCalc.py
@@ -1,2 +1,15 @@
 from sympy import *
 from sympy.abc import x, Y
+
+def MatrixOps(OPS: list[str]) -> str:
+  if 'kron' in OPS:
+    return '''__ResultName1__ = __MatrixName1*transpose(__MatrixName2__)
+  __ResultName2__ = transpose(__MatrixName1__)*__MatrixName2__
+  __ResultName3__ = kronecker_product(__ResultName1__, __ResultName2__)
+  print(trace(__ResultName1__))
+  print(trace(__ResultName3__))'''
+  else:
+    return '''__ResultName1__ = __MatrixName1*transpose(__MatrixName2__)
+  __ResultName2__ = transpose(__MatrixName1__)*__MatrixName2__
+  print(max(__ResultName1__))
+  print(sum(__ResultName2__))'''
