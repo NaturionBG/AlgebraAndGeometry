be0030922dee70175c94d4ba3bb25a9b4df37161
Naturion <naturionprodudu@mail.ru>
2024-12-30 15:26:05 +0300

    Bettered matrixops with f-string

--- MatCalc.py
+++ MatCalc.py
@@ -1,15 +1,16 @@
 from sympy import *
 from sympy.abc import x, Y
 
-def MatrixOps(OPS: list[str]) -> str:
+def MatrixOps(__MatrixName1__: str, __MatrixName2__: str, OPS: list[str]) -> str:
   if 'kron' in OPS:
-    return '''__ResultName1__ = __MatrixName1*transpose(__MatrixName2__)
-  __ResultName2__ = transpose(__MatrixName1__)*__MatrixName2__
+    return f'''__ResultName1__ = {__MatrixName1__}*transpose({__MatrixName2__})
+  __ResultName2__ = transpose({__MatrixName1__})*{__MatrixName2__}
   __ResultName3__ = kronecker_product(__ResultName1__, __ResultName2__)
   print(trace(__ResultName1__))
   print(trace(__ResultName3__))'''
   else:
-    return '''__ResultName1__ = __MatrixName1*transpose(__MatrixName2__)
-  __ResultName2__ = transpose(__MatrixName1__)*__MatrixName2__
+    return f'''__ResultName1__ = {__MatrixName1__}*transpose({__MatrixName2__})
+  __ResultName2__ = transpose({__MatrixName1__})*{__MatrixName2__}
   print(max(__ResultName1__))
   print(sum(__ResultName2__))'''
+  
