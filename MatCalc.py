ea1620fea6c8b6425c22dfaa1ec1c2354ed115ab
Naturion <naturionprodudu@mail.ru>
2024-12-30 14:49:10 +0300

    Error fixed

--- MatCalc.py
+++ MatCalc.py
@@ -1,5 +1,5 @@
 from sympy import *
-from sympy.abc import x, Y
+from sympy.abc import x, y
 
 def gaussian_elim_3vars(__MatrixName__: Matrix) -> str:
   return f'pretty_print({__MatrixName__}.rref())'
