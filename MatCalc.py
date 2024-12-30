b453a95919a7346bd2264cc14b9fb1a231bbf5e6
Naturion <naturionprodudu@mail.ru>
2024-12-30 15:06:41 +0300

    Added a function that returns the code for the echelon form of a matrix with parameters

--- MatCalc.py
+++ MatCalc.py
@@ -1,6 +1,9 @@
 from sympy import *
 from sympy.abc import x, y
 
-def gaussian_elim_3vars(__MatrixName__: Matrix) -> str:
-  return f'pretty_print({__MatrixName__}.rref())'
+def gaussian_elim() -> str:
+  return f'pretty_print(__MatrixName__.rref())'
 
+def gaussian_elim_parameters() -> str:
+  return f'pretty_print(__MatrixName__.echelon_form())
+Если Необходимо решить непростое уравнение в echelon_form - функция solve() поможет.'
