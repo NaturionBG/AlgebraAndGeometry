51c8c27d8e4ee6cb8b306674dc35d8abfeace011
Naturion <naturionprodudu@mail.ru>
2024-12-30 22:29:44 +0300

    Added a solution for Cartesian geometry

--- MatCalc.py
+++ MatCalc.py
@@ -12,3 +12,25 @@ print(im(Z))
 print(re(solutionName[x]))
 '''
 
+
+def Cartesian(Point1: str, Point2: str, Point3: str) -> str:
+  return f'''
+PointName1={Point1}
+PointName2={Point2}
+PointName3={Point3}
+PointName4=Matrix([x, y])
+LineName1=PointName3-PointName2
+LineName2=PointName4-PointName1
+VarName1, VarName2 = symbols('VarName1, VarName2')
+__eqName1__=Eq(VarName1*PointName2[0] + VarName2, PointName2[1])
+__eqName2__=Eq(VarName1*PointName3[0] + VarName2, PointName2[1])
+solutionName1=solve([__eqName1__, __eqName2__])
+__eqName3__=Eq(solutionName1[VarName1]*x + solutionName1[VarName2], y)
+__eqName4__=Eq(LineName2.dot(LineName1), 0)
+solutionName2=solve([__eqName3__, __eqName4__])
+LineName2=LineName2.subs(solutionName2)
+VarName3=(LineName2.dot(LineName2))**0.5
+print(solutionName2[x].n())
+print(solutionName2[y].n())
+print(VarName3)
+'''
