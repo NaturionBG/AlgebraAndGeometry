ac0ae664cae46e0f20780979ab6cc5fc2b59b0e9
Naturion <naturionprodudu@mail.ru>
2024-12-30 18:40:22 +0300

    done some adjustments

--- MatCalc.py
+++ MatCalc.py
@@ -2,8 +2,10 @@ from sympy import *
 from sympy.abc import x, y
 
 def gaussian_elim(__MatrixName__: str) -> str:
-  return f'pretty_print({__MatrixName__}.rref())'
+  return f'''
+pretty_print({__MatrixName__}.rref())'''
 
 def gaussian_elim_parameters(__MatrixName__: str) -> str:
-  return f'''pretty_print({__MatrixName__}.echelon_form())
+  return f'''
+pretty_print({__MatrixName__}.echelon_form())
 Если Необходимо решить непростое уравнение в echelon_form - функция solve() поможет.'''
