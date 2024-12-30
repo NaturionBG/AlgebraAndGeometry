9457110a6db4a5bf840a34f712a4777fc3cdc872
Naturion <naturionprodudu@mail.ru>
2024-12-30 15:27:16 +0300

    Bettered Gaussian elimination with f-string

--- MatCalc.py
+++ MatCalc.py
@@ -1,9 +1,9 @@
 from sympy import *
 from sympy.abc import x, y
 
-def gaussian_elim() -> str:
-  return f'pretty_print(__MatrixName__.rref())'
+def gaussian_elim(__MatrixName__: str) -> str:
+  return f'pretty_print({__MatrixName__}.rref())'
 
-def gaussian_elim_parameters() -> str:
-  return '''pretty_print(__MatrixName__.echelon_form())
+def gaussian_elim_parameters(__MatrixName__: str) -> str:
+  return f'''pretty_print({__MatrixName__}.echelon_form())
 Если Необходимо решить непростое уравнение в echelon_form - функция solve() поможет.'''
