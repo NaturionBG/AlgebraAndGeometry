e8cf8664ffad7ede68f99227a73fde985ecf787d
Naturion <naturionprodudu@mail.ru>
2025-01-05 19:20:49 +0300

    changed solution principle for gaussian elimination

--- MatCalc.py
+++ MatCalc.py
@@ -1,19 +1,12 @@
 from sympy import *
 from sympy.abc import x, y
 
-def gaussian_elim(__MatrixName__: str) -> str:
-  return f'''
+def gaussian_elim(num:int, __MatrixName__: str) -> str:
+  return [f'''
   pretty_print({__MatrixName__}.rref())
-  '''
+  '''][num-1]
 
-def gaussian_elim_parameters(__MatrixName__: str) -> str:
-  return f'''pretty_print({__MatrixName__}.echelon_form())
+def gaussian_elim_parameters(num:int, __MatrixName__: str) -> str:
+  return [f'''pretty_print({__MatrixName__}.echelon_form())
 Если Необходимо решить непростое уравнение в echelon_form - функция solve() поможет.
-
-
-1) Исследуйте следующую систему уравнений на наличие "лишних" уравнений
-pretty_print(solve({__MatrixName__}.det()))
-
-2) Найдите количество решений следующей системы уравнений при различных значениях параметра
-pretty_print(solve({__MatrixName__}.det())) <--- При этом параметре кол-во решений A.rank() - 1. В остальных случаях A.rank()
-'''
+'''][num-1]
