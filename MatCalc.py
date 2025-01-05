f72781b907516468f4f56fd8c0cc0c320e139588
Naturion <naturionprodudu@mail.ru>
2025-01-05 19:13:46 +0300

    Changed solution principle

--- MatCalc.py
+++ MatCalc.py
@@ -1,45 +1,45 @@
 from sympy import *
 from sympy.abc import x, Y
 
-def MatrixOps(__MatrixName1__: str, __MatrixName2__: str, OPS: list[str]) -> str:
+def MatrixOps(num:int, __MatrixName1__: str, __MatrixName2__: str, OPS: list[str]) -> str:
   if 'kron' in OPS:
-    return f'''
+    return [f'''
 __ResultName1__ = {__MatrixName1__}*transpose({__MatrixName2__})
 __ResultName2__ = transpose({__MatrixName1__})*{__MatrixName2__}
 __ResultName3__ = kronecker_product(__ResultName1__, __ResultName2__)
 print(trace(__ResultName1__))
-print(trace(__ResultName3__))'''
+print(trace(__ResultName3__))'''][num-1]
   else:
-    return f'''
+    return [f'''
 __ResultName1__ = {__MatrixName1__}*transpose({__MatrixName2__})
 __ResultName2__ = transpose({__MatrixName1__})*{__MatrixName2__}
 print(max(__ResultName1__))
-print(sum(__ResultName2__))'''
+print(sum(__ResultName2__))'''][num-1]
   
-def MatrixEquations(name1: str, name2: str, name3: str = None) -> str:
+def MatrixEquations(num: int, name1: str, name2: str, name3: str = None) -> str:
   if name3!=None:
-    return f'''
-pretty_print({name1}.inv()*{name3}*({name2}.inv()))'''
+    return [f'''
+pretty_print({name1}.inv()*{name3}*({name2}.inv()))'''][num-1]
   else:
-    return f'''
+    return [f'''
 Если у вас вид: AX=B, то код: pretty_print({name1}.inv()*{name2})
-Если же вид: XA=B, то код: pretty_print({name2}*({name1}.inv()))'''
+Если же вид: XA=B, то код: pretty_print({name2}*({name1}.inv()))'''][num-1]
     
-def Deter(name1: str, name2: str = None) -> str:
+def Deter(num:int, name1: str, name2: str = None) -> str:
   if name2!=None:
-    return f'''
+    return [f'''
 name3={name1}*{name2}
 __equationName__ = Eq(det(name3), 0)
 VarName=solve(__equationName__)
 В оформлении задания на бумаге самостоятельно найдите, линейной комбинацией 
-каких строк/столбцов является столбец/строка с параметром'''
+каких строк/столбцов является столбец/строка с параметром'''][num-1]
   else:
-    return f'''
+    return [f'''
 pretty_print(det({name1}))
-В оформлении задания на бумаге распишите, как вы находили определитель.'''
+В оформлении задания на бумаге распишите, как вы находили определитель.'''][num-1]
     
-def Permute(permName: str, index: int, btrace: int) -> str:
-  return f'''
+def Permute(permName: str, index: int, btrace: int, num: int) -> str:
+  return [f'''
 name=Matrix([])
 for i in {permName}:
   name=name.hstack(name, Matrix([0 if k+1!=i else 1 for k in range(len(perm))]))
@@ -49,12 +49,12 @@ print(name**{index})
 for varName in range(1, 100):
   print(name**varName, varName)
   if name**varName == {btrace}:
-    break'''
+    break'''][num-1]
       
-def BigMatrices(order: int, diagstart: int, diagend: int, a12: int = None, a21: int = None, row1end: int = None) -> str:
+def BigMatrices(num: int, order: int, diagstart: int, diagend: int, a12: int = None, a21: int = None, row1end: int = None) -> str:
   if row1end!=None:
-    return f'''
-# Первое решение:\n    
+    return [f'''
+# Первое решение:\n   
 
 VarName1={order} 
 Name1={diagstart}
@@ -71,9 +71,11 @@ inversed_diagListName=[1/i for i in diagonListName]
 inversed_rowListName=[-i/k for i, k in zip(rowListName, diagonListName)]
 print(sum(inversed_diagListName)+sum(inversed_rowListName[1:]))
 print(min([min(inversed_diagListName), min(inversed_rowListName)]))
-    '''
+'''][num-1]
   else:
-    return f'''
+    return [f'''
+# Первое решение:\n 
+            
 VarName1={order}
 Name2={diagstart}
 name3={diagend}
@@ -93,7 +95,9 @@ listName2=[1/i for i in listName]
 MatrixName2=MatrixName.inv()
 
 print(VarName3, trace(MatrixName2)+sum(listName2))
+''',
 
+f'''
 # Второе решение:\n
 
 
@@ -113,11 +117,11 @@ for i in spisochek[2:]:
   
 print(round(diagonal,3))
 print(round(sled,3))
-'''
+'''][num-1]
     
     
-def MatrixRows(StarterMatrixName: str, symb: float, m: int, n: int, f: int, lesserThan: float) -> str:
-  return f'''
+def MatrixRows(StarterMatrixName: str, symb: float, sm: int, sn: int, bm: int, lesserThan: float, num: int) -> str:
+  return [f'''
 Первое решение:\n
   
 __MatrixName__={symb}*{StarterMatrixName}
@@ -135,13 +139,13 @@ for k in range(300):
   if max(FunctionName2(k))<{lesserThan}:
     print(k)
     break
-print(max(FunctionName1({m}, {n})), max(FunctionName2({f})), VarName4)
+print(max(FunctionName1({sm}, {sn})), max(FunctionName2({bm})), VarName4)
 
 # В данной задаче необходимо (В оформлении) доказать, что матрица S сходится к B.
-# Комментарий к вышеизложенному: Егор, это просто жесть
 
+''',
+f'''
 # Второе решение:\n
-
 __MatrixName__={symb}*{StarterMatrixName}
 
 def s(m,n):
@@ -154,15 +158,15 @@ def b(m):
     B = ((I - A).inv()) * (A**m)
     return B
   
-print(max(s({m}, {n})))
-print(max(b({f})))
+print(max(s({sm}, {sn})))
+print(max(b({bm})))
 
 
 for i in range(500):
     if max(b(i)) < {lesserThan}:
         print(i)
         break
-'''
+'''][num-1]
 
 
 def arifm_and_geom_matrix_progression(n: int, a11: float, a1n: float, ann: float):
@@ -186,3 +190,4 @@ for i in range(vsego_symbols-1):
     spis_stroki_B.append(-spis_stroki[1:][i] / spis_diag[1:][i])
 
 print(round(min(min(spis_stroki_B), min(spis_diag_B)), 3), round(sum(spis_diag_B) + sum(spis_stroki_B) - 1, 3))'''
+
