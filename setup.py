456863df45fb296da767584600607e34f08b906a
Naturion <naturionprodudu@mail.ru>
2025-01-12 00:09:34 +0300

    New changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scumpy',
-    version=1.1,
+    version=1.2,
     packages=find_packages(),
     install_requires=[
         'sympy>=1.13.1'
