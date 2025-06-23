7652174ccc0d5e19cd9da291d746072704a43bd2
Naturion <naturionprodudu@mail.ru>
2025-06-23 19:02:46 +0300

    Wheel generated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scumpy',
-    version=2.55,
+    version=2.6,
     packages=find_packages(),
     install_requires=[
         'sympy>=1.13.1'
